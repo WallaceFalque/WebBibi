be5abf217e2584cdf7402780daeb1cec7a3d1900
Wallace Falque <wallaceffalque@gmail.com>
2025-12-15 20:41:52 -0300

    Adicionar arquivos de projeto.

---
+++ WebBibi/Models/VeiculoCardDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace WebBibi.Models
+{
+    public class VeiculoCardDTO
+    {
+        public string ImagemUrl { get; set; } = string.Empty;
+        public string Titulo { get; set; } = string.Empty;
+        public string Versao { get; set; } = string.Empty;
+        public string Cambio { get; set; } = string.Empty;
+        public string Preco { get; set; } = string.Empty;
+        public string Ano { get; set; } = string.Empty;
+        public string Quilometragem { get; set; } = string.Empty;
+        public string Localizacao { get; set; } = string.Empty;
+    }
+}
