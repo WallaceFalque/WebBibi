979350813119708b1ef30e1e48b614f1c00abe8a
Wallace Falque <wallaceffalque@gmail.com>
2025-12-24 09:51:08 -0300

    Alteraçoes na buscaAvuslo + Redesign do card avuslo criação das classes do card avuslo e população fake por lista

--- WebBibi/Models/VeiculoCardDTO.cs
+++ WebBibi/Models/VeiculoCardDTO.cs
@@ -11,4 +11,16 @@
         public string Quilometragem { get; set; } = string.Empty;
         public string Localizacao { get; set; } = string.Empty;
     }
+
+    public class Veiculo
+    {
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public decimal Preco { get; set; }
+        public bool EhOferta { get; set; }
+        public bool AceitaUsado { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+
 }
