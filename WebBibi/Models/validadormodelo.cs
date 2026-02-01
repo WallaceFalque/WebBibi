6793339ee6233465175a85dc8ee724742626980c
wallace <wallace@DESKTOP-LQM1JO7>
2026-02-01 09:00:21 -0300

    Reapply "mudança no form de curriculo e criação do validador ( preciso estudar mais blazor)"
    This reverts commit a9ca540887c7d28095f6c9f970f9e3898b15e963.

---
+++ WebBibi/Models/validadormodelo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBibi.Models
+{
+    public class ValidadorModelo
+    {
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Digite um e-mail válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O celular é obrigatório.")]
+        [RegularExpression(@"^\(?\d{2}\)?[\s-]?9?\d{4}[\s-]?\d{4}$",
+            ErrorMessage = "Formato inválido. Use (XX) 9XXXX-XXXX.")]
+        public string Celular { get; set; }
+        public string Nome { get; set; }
+    }
+}
